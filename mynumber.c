7231ad089bff55d01145fab6a6cb70c1804646ae
wannabidr <sungwooki9@naver.com>
2025-04-07 13:44:31 +0000

    mynumber added prime

--- mynumber.c
+++ mynumber.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+bool is_prime(int n) {
+	if (n <= 1) return false;
+	for (int i=2; i*i<=n; ++i) {
+		if (n%i == 0) return false;
+	}
+	return true;
+}
+
 int main() {
 	for(int i=0; i<100; i++) {
 		if(i+1 % 2 == 1) {
@@ -11,6 +19,11 @@ int main() {
 		if(i+1 % 2 == 0) {
 			printf(i+1);
 			printf("\n");
+		}
+	}
+	for (int i=0; i<100; i++) {
+		if(is_prime(i+1)) {
+			printf("%d\n", i+1);
 		}
 	}
 	return 0;
